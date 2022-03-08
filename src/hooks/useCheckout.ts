cb47decfd95050a77e9b6aed2be082db3c003f5f
Michael Ossig <mrossig201@gmail.com>
2022-03-08 14:57:30 -0500

    added notes

--- src/hooks/useCheckout.ts
+++ src/hooks/useCheckout.ts
@@ -30,6 +30,7 @@ export const useCheckout = (options: {
     // creates ref and uses data compare callback to decide if re-rendering should occur.  Without this, there is an infinite loop.
     const opts = useMemoCompare(options, compareSessionData);
 
+    // TODO: This config will be brought in from front end. Add as argument above
     useEffect(() => {
         const { sessionId, sessionData, redirectResult } = options;
 
