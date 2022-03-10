cca2b02b641585cabf0e928b698c90e95f1f1a33
Michael Ossig <mrossig201@gmail.com>
2022-03-10 14:31:05 -0500

    removed test server from client, updated server index to point to new dir names

--- packages/client/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 8080,
-    API_KEY: process.env.API_KEY || '123456',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
-};
--- packages/server/index.js
+++ packages/server/index.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-const root = path.join(__dirname, '../adyen-demo-client', 'build');
+const root = path.join(__dirname, '../client', 'build');
 app.use(express.static(root));
 app.get('*', (req, res) => {
   res.sendFile('index.html', { root });
