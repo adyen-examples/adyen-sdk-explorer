7a5104e9234b86562be7bdf87cf388ce250ff406
Michael Ossig <mrossig201@gmail.com>
2022-12-02 14:22:14 -0500

    updated resources route and changed properties type to array

--- packages/server/routes/resources/index.ts
+++ packages/server/routes/resources/index.ts
@@ -17,11 +17,16 @@ router.get('/', (req: Request, res: Response) => {
   }
 });
 
-router.get('/:paymentMethod', (req: Request, res: Response) => {
+router.post('/', (req: Request, res: Response) => {
   try {
     const paymentMethodProps: PaymentMethodProperties = paymentMethodProperties;
-    const paymentMethodToSend = paymentMethodProps[req.params.paymentMethod];
-    return res.status(201).json(paymentMethodToSend);
+    const paymentMethods: PaymentMethodProperties = {};
+
+    req.body.paymentMethods.forEach((pm: string) => {
+      paymentMethods[pm] = paymentMethodProps[pm];
+    });
+
+    return res.status(201).json(paymentMethods);
   } catch (err) {
     res.status(500).json({ message: 'Internal server error' });
   }
